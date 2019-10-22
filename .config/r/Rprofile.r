f282bdbbbaf24ba205234627035cd17936908cb3
ndwarshuis <ndwar@yavin4.ch>
2019-10-22 00:41:03 -0400

    fix links in config

--- .config/r/Rprofile.r
+++ .config/r/Rprofile.r
@@ -1,1 +1,10 @@
-../../.homesick/repos/dotfiles/home/.config/r/Rprofile.r
+.First <- function() {
+##  message("R is the best\n","working directory is:", getwd())
+}
+
+## Set CRAN mirror:
+local({
+  r <- getOption("repos")
+  r["CRAN"] <- "https://cloud.r-project.org/"
+  options(repos = r, Ncpus = 8)
+})
